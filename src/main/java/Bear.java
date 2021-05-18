07fc4f5ff53bc76023eae490014da4e87e151794
Martin Sutherland <martinsutherland1@gmail.com>
2021-05-18 14:57:04 +0100

    github save

---
+++ src/main/java/Bear.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Bear {
+
+    private String name;
+    private ArrayList<Salmon> belly;
+
+    public Bear(String name){
+        this.name = name;
+        this.belly = new ArrayList<Salmon>();
+    }
+
+
+    public int foodCount() {
+        return this.belly.size();
+    }
+
+    public void eatFishFromRiver(River river) {
+        Salmon salmon = river.removeFish();
+        this.belly.add(salmon);
+    }
+
+    public void sleep() {
+        this.belly.clear();
+    }
+}
---
+++ src/test/java/BearTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BearTest {
+
+    private Bear bear;
+    private Salmon salmon;
+    private River river;
+
+
+    @Before
+    public void before(){
+        bear = new Bear("Sooty");
+        salmon = new Salmon();
+        river = new River();
+        river.addFish(salmon);
+    }
+
+    @Test
+    public void bellyStartsEmpty(){
+        assertEquals(0, bear.foodCount());
+    }
+
+    @Test
+    public void canEatSalmon(){
+        bear.eatFishFromRiver(river);
+        assertEquals(1, bear.foodCount());
+    }
+
+    @Test
+    public void shouldEmptyBellyAfterSleeping(){
+        bear.eatFishFromRiver(river);
+        bear.sleep();
+        assertEquals(0, bear.foodCount());
+    }
+
+}
